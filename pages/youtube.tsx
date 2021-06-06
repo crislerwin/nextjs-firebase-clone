ee97568acc6b833413d051db2c35c9eadc35a865
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-06-05 21:07:01 -0300

    chore: add index page for project sandbox

--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,6 +1,6 @@
 import Layout from "./src/Components/Youtube/Layout";
 import { Button } from "@material-ui/core";
-export default function App() {
+export default function Youtube() {
   return (
     <Layout>
       Clone do Youtube com Next.js
