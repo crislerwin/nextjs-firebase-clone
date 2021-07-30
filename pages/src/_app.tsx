a429e38e2c05054a9104b24040f6719da4827c8b
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-07-30 18:25:38 -0300

    chore: add types to MyApp props

--- pages/src/_app.tsx
+++ pages/src/_app.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
+import { AppProps } from "next/app";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
 
-export default function MyApp(props: any) {
+export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
 
   React.useEffect(() => {
