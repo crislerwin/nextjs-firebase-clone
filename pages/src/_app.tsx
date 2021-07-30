1d3908272fa27ebe303eaaff9fc682c8e613de6f
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-07-30 19:53:57 -0300

    🐛 fix app build error

--- pages/src/_app.tsx
+++ pages/src/_app.tsx
@@ -1,41 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Head from "next/head";
-import { AppProps } from "next/app";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { theme } from "./theme";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import type { AppProps } from "next/app";
+import { theme } from "../src/theme";
+import { useEffect } from "react";
 
-export default function MyApp(props: AppProps) {
-  const { Component, pageProps } = props;
-
-  React.useEffect(() => {
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles: any = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
   }, []);
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </MuiThemeProvider>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
